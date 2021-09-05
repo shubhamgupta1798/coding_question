cd49725fc494efecd9da9eb31d2ab1a82606e298
shubhamgupta1798 <31383761+shubhamgupta1798@users.noreply.github.com>
2021-09-04 20:53:56 -0500

    Update merge-sorted-array.cpp

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> c;
-        int i=0;
-        int j1=0;
-        int j2=0;
-    
-        while(i!=m+n)
+        int m0=m+n-1;
+        while(m!=0 || n!=0)
         {
-            i++;
-            if(j1==m)
+            if(m==0)
             {
-                c.push_back(nums2[j2]);
-                j2++;
+                nums1[m+n-1]=nums2[n-1];
+                n--;
             }
-            else if(j2==n)
+            else if(n==0)
             {
-                c.push_back(nums1[j1]);
-                j1++;
+                nums1[m+n-1]=nums1[m-1];
+                m--;
             }
-            else if(nums1[j1]<nums2[j2] && j1<m)
+            else if(nums1[m-1]>nums2[n-1])
             {
-                c.push_back(nums1[j1]);
-                j1++;
+                nums1[m+n-1]=nums1[m-1];
+                m--;
             }
-            else{
-                c.push_back(nums2[j2]);
-                j2++;
+            else
+            {
+                nums1[m+n-1]=nums2[n-1];
+                n--;
             }
         }
-        nums1=c;
         return ;
     }
 };
