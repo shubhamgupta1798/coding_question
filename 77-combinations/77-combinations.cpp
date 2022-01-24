93e0dbcf2ea86bd7ce88688a94e090adbf73ba20
shubhamgupta1798 <31383761+shubhamgupta1798@users.noreply.github.com>
2022-01-23 23:55:36 -0600

    Time: 63 ms (28.18%), Space: 18.4 MB (31.27%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void dfs(int prev,int n,vector<int> v, int k)
+    void dfs(int prev,int n,vector<int> v, int k,int depth)
     {
         if(v.size()==k)
         {
@@ -13,12 +13,12 @@ public:
         }
         else 
         {
-            for(int i=prev+1;i<=n;i++)
+            for(int i=prev+1;i<=n-k+depth+1;i++)
             {
                 vector<int> v2;
                 v2=v;
                 v2.push_back(i);
-                dfs(i,n,v2,k);
+                dfs(i,n,v2,k,depth+1);
             }
             return;
         }
@@ -26,11 +26,11 @@ public:
     vector<vector<int>> combine(int n, int k) {
         
         vector<int> v;
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=n-k+1;i++)
         {
              vector<int> v;
             v.push_back(i);
-            dfs(i,n,v,k);
+            dfs(i,n,v,k,1);
         }
         return ans;
     }
